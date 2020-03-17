12e9f32c47634bf61bec6cd1f1046636f7cd02a8
Sandai <42770217+Sandaidev@users.noreply.github.com>
2020-03-17 20:11:10 +0100

    big changes to lib_ja.php
    - table_sensors_name is now defined
    - table_history_name is now defined
    - both should now declared as globals in every function
    - fixed the database initialization bug
    - finished the initialize_database() function
    - Added the check_if_db_exists() function
    - Added the inject_sensors_data_into_db() function, which should be used by the Arduino
    - Added the insert_date_into_stats() function, which should add a new row into the history table
    - Added the truncate_history_table() function, that should be used if the end user wants to empty the history table.

--- assets/lib/lib_jardin_autonome.php
+++ assets/lib/lib_jardin_autonome.php
@@ -5,7 +5,8 @@ $db_username = "root";
 $db_password = "";
 
 $db_name = "jardin";
-
+$table_sensors_name = "capteurs";
+$table_history_name = "historique";
 
 
 function initialize_database()
@@ -20,6 +21,8 @@ function initialize_database()
     global $db_username;
     global $db_password;
     global $db_name;
+    global $table_sensors_name;
+    global $table_history_name;
 
     // Create and initialize a new mysqli object (Oriented Object amirite)
     $db_connection = new mysqli($db_servername, $db_username, $db_password);
@@ -38,7 +41,105 @@ function initialize_database()
     $create_db_query = "CREATE DATABASE jardin";
 
     // Send the query and stop the script if an error happened
-    if ($db_connection->query($create_db_query) != false) {
+    if ($db_connection->query($create_db_query) != true) {
         die("Error creating database! Details: " . $db_connection->error);
     }
+
+    // We now need to create the tables
+    mysqli_select_db($db_connection, $db_name);
+
+    $create_table_sensors_query = "CREATE TABLE `$table_sensors_name` (
+        `CUVE1` varchar(10) NOT NULL,
+        `CUVE2` varchar(10) NOT NULL,
+        `CUVE3` varchar(10) NOT NULL,
+        `CUVE4` varchar(10) NOT NULL,
+        `HUMIDITE` varchar(10) NOT NULL
+      )";
+
+    $create_table_history_query = "CREATE TABLE `$table_history_name` (
+        `mois` varchar(10) DEFAULT NULL,
+        `jour` varchar(10) DEFAULT NULL,
+        `heure` varchar(10) DEFAULT NULL
+      )";
+
+    $db_connection->query($create_table_history_query);
+    $db_connection->query($create_table_sensors_query);
+
+    return true;
+
+    $db_connection->close();
+}
+
+
+function check_if_db_exists()
+{
+    global $db_servername;
+    global $db_username;
+    global $db_password;
+    global $db_name;
+
+    $db_connection = new mysqli($db_servername, $db_username, $db_password);
+
+    if (mysqli_select_db($db_connection, $db_name) != true) {
+        return false;
+    } else {
+        return true;
+    }
+
+    $db_connection->close();
+}
+
+function inject_sensors_data_into_db($cuve_1, $cuve_2, $cuve_3, $cuve_4, $humidite)
+{
+    /*
+    * We only need to change the values of the sensors table,
+    * So we use the UPDATE request and not INSERT.
+    */
+
+    global $db_servername;
+    global $db_username;
+    global $db_password;
+    global $table_sensors_name;
+
+    $db_connection = new mysqli($db_servername, $db_username, $db_password);
+    $inject_data_query = "UPDATE `$table_sensors_name` SET `CUVE1`=$cuve_1,`CUVE2`=$cuve_2,`CUVE3`=$cuve_3,`CUVE4`=$cuve_4,`HUMIDITE`=$humidite WHERE 1";
+
+    $db_connection->query($inject_data_query);
+    $db_connection->close();
+}
+
+function insert_date_into_stats($month, $day, $hour)
+{
+    /*
+    * We insert a new date in the history table.
+    */
+
+    global $db_servername;
+    global $db_username;
+    global $db_password;
+    global $table_history_name;
+
+    $db_connection = new mysqli($db_servername, $db_username, $db_password);
+    $insertion_query = "INSERT INTO `$table_history_name`(`mois`, `jour`, `heure`) VALUES ($month,$day,$hour)";
+
+    $db_connection->query($insertion_query);
+    $db_connection->close();
+}
+
+function truncate_history_table()
+{
+    /*
+    * Empties the history table
+    */
+
+    global $db_servername;
+    global $db_username;
+    global $db_password;
+    global $table_history_name;
+
+    $db_connection = new mysqli($db_servername, $db_username, $db_password);
+    $truncate_query = "TRUNCATE TABLE $table_history_name";
+
+    $db_connection->query($truncate_query);
+    $db_connection->close();
 }
