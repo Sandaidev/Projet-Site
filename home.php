e8a0e1cbdd50b88b65ed3a9ed1dff62a94b1e9d1
Sandai <42770217+Sandaidev@users.noreply.github.com>
2020-03-17 12:32:00 +0100

    Create about, home + clean up HTML

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Accueil - Jardin Autonome</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="logo">
+            <a href="./home.php" class="logo"><strong>JARDIN AUTONOME</strong></a>
+        </div>
+
+        <div class="header-right">
+            <a class="header-right active" href="./home.php">Accueil</a>
+            <a class="header-right" href="./stats.php">Stats</a>
+            <a class="header-right" href="./capteurs.php">Capteurs</a>
+            <a class="header-right" href="./about.html">À propos</a>
+        </div>
+    </div>
+</body>
+
+</html>
