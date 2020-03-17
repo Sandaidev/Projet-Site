3942d5e719bada2b297086c7330973c1fe27424b
Sandai <42770217+Sandaidev@users.noreply.github.com>
2020-03-17 21:47:03 +0100

    fix typo

--- debug.php
+++ debug.php
@@ -64,9 +64,9 @@
 
     require_once "./assets/lib/lib_jardin_autonome.php";
 
-    // On check si les variables sont définies
+    // On check si au moins une variable est définie
     if (isset($_GET['cuve1']) && !empty($_GET['cuve1'])) {
-        // Toutes les variables sont définies, on peut ajouter à la BDD!
+        // On peut ajouter à la BDD!
         inject_sensors_data_into_db($_GET['cuve1'], $_GET['cuve2'], $_GET['cuve3'], $_GET['cuve4'], $_GET['humidite']);
     }
 
