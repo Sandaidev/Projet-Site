8a0ea64c8a1ae95abcb715e3d0d3b89b6a9841a4
Sandai <42770217+Sandaidev@users.noreply.github.com>
2020-03-18 12:38:41 +0100

    Some changes to index.php
    - The server will now check if the DB exists right after the session_start() function
    - The page will auto-redirect the user to the home page if its credentials are valid
    - Changed header href to index.php
    - login form now sends login values to self
    - Added a maxlength to the password input to 64 characters, in order to match the one in the database
    - If the creds submitted are valid, we now store them inside the $_SESSION superglobal and redirect the user to the home page

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+session_start();
+require_once "./assets/lib/lib_jardin_autonome.php";
+
+// On regarde si la BDD existe
+if (check_if_db_exists() == false) {
+    initialize_database();
+
+    echo "<div class='footer'>
+        <h4>
+        Il semblerait que vous vous connectez pour la première fois,
+        <br>
+        vôtre nom d'utilisateur est '<em>jardin</em>' et vôtre mot de passe est '<em>autonome</em>'</h4>
+        </div>";
+}
+
+if (check_if_session_is_valid($_SESSION) == true) {
+    echo "<script>window.location.replace('home.php');</script>";
+}
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -12,7 +33,7 @@
 
     <div class="header">
         <div class="logo">
-            <a href="./home.php" class="logo"><strong>JARDIN AUTONOME</strong></a>
+            <a href="./index.php" class="logo"><strong>JARDIN AUTONOME</strong></a>
         </div>
 
     </div>
@@ -21,17 +42,16 @@
         <h2>Login</h2>
         <hr>
 
-        <form action="./home.php" method="POST">
+        <form method="POST">
             <table>
                 <tr>
-
                     <td><label for="username">Username - </label></td>
                     <td><input type="text" required id="username" name="username" size="16" maxlength="16"></td>
                 </tr>
 
                 <tr>
                     <td><label for="password">Password - </label></td>
-                    <td><input type="password" required id="password" name="password" size="16"></td>
+                    <td><input type="password" required id="password" name="password" size="16" maxlength="64"></td>
                 </tr>
 
                 <br>
@@ -42,27 +62,24 @@
 
         <a href="./nukedb.php">Mot de passe oublié</a>
 
-        <?php
-
-        // Importation des librairies
-        require_once "./assets/lib/lib_jardin_autonome.php";
-
-        // On regarde si la BDD existe
-        if (check_if_db_exists() == false) {
-            initialize_database();
-
-            echo "<div class='footer'>
-            <h4>
-            Il semblerait que vous vous connectez pour la première fois,
-            <br>
-            vôtre nom d'utilisateur est '<em>jardin</em>' et vôtre mot de passe est '<em>autonome</em>'</h4>
-            </div>";
-        }
-
-        ?>
-
     </div>
 
+    <?php
+
+    // Importation des librairies
+    require_once "./assets/lib/lib_jardin_autonome.php";
+
+    // On regarde si les credentials sont disponibles dans $_POST
+    if (isset($_POST['username']) && !empty($_POST['username'])) {
+        // On les compare à la BDD
+        if (check_if_creds_are_valid($_POST['username'], $_POST['password']) == true) {
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['password'] = $_POST['password'];
+            echo "<script>window.location.replace('home.php');</script>";
+        }
+    }
+
+    ?>
 
 </body>
 
