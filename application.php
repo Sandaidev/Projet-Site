f2e354d777f0194469c3fd27c8bc8d5798951450
Sandai <42770217+Sandaidev@users.noreply.github.com>
2020-03-20 18:27:50 +0100

    Pretty-print JSON API

--- application.php
+++ application.php
@@ -3,6 +3,7 @@
 require_once "./assets/lib/lib_jardin_autonome.php";
 
 $db_data = return_formatted_sensor_table();
-$app_json = json_encode($db_data);
+$app_json = json_encode($db_data, JSON_PRETTY_PRINT);
 
+header("Content-Type: application/json");
 echo $app_json;
